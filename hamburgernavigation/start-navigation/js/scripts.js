51d37f18591a3799a5fd526b6e5815bc52b926c9
Kiser09 <kiser09@byui.edu>
2022-01-28 11:30:02 -0900

    committ changes to the hamburger menus

---
+++ hamburgernavigation/start-navigation/js/scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    document.getElementById('primarynav').classList.toggle("open");
+    document.getElementById('hamburgerbtn').classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerbtn');
+x.onclick = toggleMenu;
