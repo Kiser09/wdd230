67a52018128fb1aba7b5d00d049bc3d6ff5984af
Kiser09 <kiser09@byui.edu>
2022-01-28 22:59:50 -0900

    added map image and added date to header

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -1,6 +1,12 @@
+const x = new Date();
+const time = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+document.getElementById('currentdate').innerHTML = x.toLocaleString('en-us', time)
+
+/*Date last modified JavaScript*/
 const lastmod = document.querySelector("#lastmod");
 lastmod.innerHTML = `Last date modified: ${document.lastModified}`;
 
+/*Current year displayed in the footer*/
 const date = new Date();
 let year = date.getFullYear();
 document.getElementById("currentyear").innerHTML = year;
