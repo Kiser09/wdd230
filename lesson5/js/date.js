77a567514bdc7c4371c8ff6ff62e40008bef62e0
Kiser09 <kiser09@byui.edu>
2022-02-04 23:05:10 -0900

    added date modified

---
+++ lesson5/js/date.js
@@ -0,0 +1,8 @@
+/*Date last modified JavaScript*/
+const lastmod = document.querySelector("#lastmod");
+lastmod.innerHTML = `Last date modified: ${document.lastModified}`;
+
+/*Current year displayed in the footer*/
+const date = new Date();
+let year = date.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
