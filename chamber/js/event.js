b28baeba088fc105123a17e12d8a04fbb0a26341
Kiser09 <kiser09@byui.edu>
2022-02-09 10:00:36 -0900

    changes to the banner display

--- chamber/js/event.js
+++ chamber/js/event.js
@@ -1,7 +1,18 @@
-const weekday = new Date()
-const day = weekday.getDay()
+const now = new Date();
 
-if(day.getDay() === 2 || day.getDay() === 1) {
-	const banner = document.querySelector("#meeting");
-	banner.style.display = "block";
+let weekday = new Array(7);
+weekday[0]= "Sunday";
+weekday[1]= "Monday";
+weekday[2]= "Tuesday";
+weekday[3]= "Wednesday";
+weekday[4]= "Thursday";
+weekday[5]= "Friday";
+weekday[6]= "Saturday";
+let day = weekday[now.getDay()];
+
+if (day = "Monday" || "Tuesday") {
+	document.getElementById("eventbanner").innerHTML = eventbanner.base.display = "block";
+} 
+else {
+	eventbanner.base.display = "none";
 }
