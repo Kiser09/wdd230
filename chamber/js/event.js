1aecf79d4eaec2870584b324ed871c2e7fafae64
Kiser09 <kiser09@byui.edu>
2022-02-09 10:46:21 -0900

    made some changes

--- chamber/js/event.js
+++ chamber/js/event.js
@@ -5,6 +5,3 @@ const day = now.getDay();
 	if (day = 1 || 2) {
 		document.querySelector('#eventbanner').style.display = 'block';
 	} 
-	else {
-		document.querySelector('#eventbanner').style.display = 'none';
-	}
