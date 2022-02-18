d3670252e3c792732a3385d7dbf1c30b57075738
Kiser09 <kiser09@byui.edu>
2022-02-17 18:49:23 -0900

    added css and everything

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,9 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]')
+
+/**Last Modified JS for the footer**/
+const lastmod = document.querySelector("#lastmod");
+lastmod.innerHTML = `Last date modified: ${document.lastModified}`;
+
+const date = new Date();
+let year = date.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
