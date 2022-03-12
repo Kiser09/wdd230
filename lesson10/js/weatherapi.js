e2b82aaddd026cf15c3c6c309059b179cd040c10
Kiser09 <kiser09@byui.edu>
2022-03-12 10:25:11 -0900

    changed apiURL

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=fairbanks,alaska&units=imperial&appid=c3cede631c229fe404795865753b8a97";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=fairbanks&units=imperial&appid=c3cede631c229fe404795865753b8a97";
 
 fetch(apiURL)
     .then((response) => response.json())
