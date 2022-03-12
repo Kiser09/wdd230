50399de48f001482b8031ad5b9d6c6bfe5c7bf50
Kiser09 <kiser09@byui.edu>
2022-03-12 11:32:35 -0900

    push changes made so far

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,3 +1,15 @@
+/*Weather API for the weather section*/
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=north+pole,alaska&units=imperial&appid=c3cede631c229fe404795865753b8a97"
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        document.querySelector('#current-temp').textContent = jsObject.main.temp.toFixed(1);
+    });
+
+/*Wind Chill function for the weather section*/
 const t = parseInt(document.querySelector("#temp").textContent);
 const w = parseInt(document.getElementById("wind").textContent);
 
