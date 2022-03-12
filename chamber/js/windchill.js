ba187bb15933e6daea567a5820330ef283a991b1
Kiser09 <kiser09@byui.edu>
2022-03-12 12:35:04 -0900

    push weather stuff

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -5,15 +5,15 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-    /* */ 
-        document.querySelector('temp').textContent = jsObject.main.temp.toFixed(1);
+    /*This document selector gathers the temp from the API and delievers the temp into the html*/ 
+        document.querySelector('#temp').textContent = jsObject.main.temp.toFixed(1);
 
-        document.querySelector('#current-temp').textContent = jsObject.main.temp.toFixed(1);
-    });
+    /*Document Selector gathers the temp from Fetch and displays into the html doc. Windchill calculation follows the wind doc selector.*/
+        document.querySelector('#wind').textContent = jsObject.wind.speed.toFixed(1);
 
-/*Wind Chill function for the weather section*/
-const t = parseInt(document.querySelector("#temp").textContent);
-const w = parseInt(document.getElementById("wind").textContent);
+        /*Wind Chill function for the weather section*/
+        const t = jsObject.main.temp.toFixed(1);
+        const w = jsObject.wind.speed;
 
 if(t <= 50 && w > 3) {
     const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(w,0.16) + 0.4275 * t * Math.pow(w,0.16)
@@ -22,3 +22,17 @@ if(t <= 50 && w > 3) {
 else {
     document.querySelector("#windchill").innerHTML = "N/A"
 }
+
+        const desc = jsObject.weather[0].description;
+        /*Sets page img alt attribute to the description variable*/
+        document.querySelector('#weathericon').setAttribute('alt', desc);
+
+        /*Weather Condition tag*/
+        document.querySelector('#condition').textContent = desc;
+
+        /*The following variable and document selector are to display the icon from the API to the html page.*/
+        const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+
+        document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    });
+
