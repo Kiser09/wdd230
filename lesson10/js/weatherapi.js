536cd37651b3eb33da02bd103757d0f8a57a4178
Kiser09 <kiser09@byui.edu>
2022-03-12 10:24:21 -0900

    made pages

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,7 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=fairbanks,alaska&units=imperial&appid=c3cede631c229fe404795865753b8a97";
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then ((jsObject) => {
+        console.log(jsObject);
+    });
