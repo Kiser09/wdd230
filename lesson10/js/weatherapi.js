f32a94e494976bd787e9d6572c365a3bf7726d55
Kiser09 <kiser09@byui.edu>
2022-03-12 10:37:05 -0900

    push changes

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,7 +1,6 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=fairbanks&units=imperial&appid=c3cede631c229fe404795865753b8a97";
-
+const apiURL = "api.openweathermap.org/data/2.5/weather?q=fairbanks&units=imperial&appid=c3cede631c229fe404795865753b8a97";
 fetch(apiURL)
-    .then((response) => response.json())
-    .then ((jsObject) => {
-        console.log(jsObject);
-    });
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+  });
