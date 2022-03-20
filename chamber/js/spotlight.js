8e681608652eae5361f6c6ff0022faa455c49ad9
Kiser09 <kiser09@byui.edu>
2022-03-19 20:27:43 -0800

    pusing this stuff

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -2,6 +2,8 @@
 
 const requestURL = 'https://kiser09.github.io/wdd230/chamber/data/data.json';
 
+const spaceFilled = 0
+
 fetch(requestURL)
     .then(function (response) {
         return response.json();
@@ -9,12 +11,32 @@ fetch(requestURL)
     .then(function (jsonObject) {
         console.table(jsonObject);
         const localbusiness = jsonObject['localbusiness'];
-        const spotlightBusiness = localbusiness.filter( x => (x.membership === "gold" || x.membership === "silver"));
-        spotlightBusiness.forEach(buildBusiness)
+        const spotlightBusiness = localbusiness.filter( x => (x.membership === 'gold' || x.membership === 'silver'));
+        const shuffledList = spotlightBusiness.sort((a, b) => 0.5 - Math.random());
+        shuffledList.forEach(spaces)
     });
 
-    function buildSpotlight(company) {
-        let card = document.createElement('section');
+    function spaces(biz) {
+        if (spaceFilled == 3)
+            return;
+        else {
+            if (spaceFilled == 0) {
+                let bizCard = document.querySelector('#spotlight1');
+                buildSpotlight(biz, bizCard);
+            }
+            if (spaceFilled == 1) {
+                let bizCard = document.querySelector('#spotlight2');
+                buildSpotlight(biz, bizCard);
+            }
+            if (spaceFilled == 2) {
+                let bizCard = document.querySelector('#spotlight3');
+                buildSpotlight(biz, bizCard);
+            }
+            spaceFilled++;
+        }
+    } 
+
+    function buildSpotlight(company, bizCard) {
         let h4 = document.createElement('h3');
         let img = document.createElement('img');
         let phone = document.createElement('p');
@@ -25,28 +47,17 @@ fetch(requestURL)
         h4.textContent = `${company.name}`;
         address.textContent = `${company.address}`;
         phone.textContent = `${company.phone}`;
-        site.innerHTML = `<a href="${company.website}">${company.website}</a>`;
+        site.innerHTML = `<a href="${company.wevbsite}">${company.website}</a>`;
 
         //image js
         img.setAttribute('src', company.logo);
         img.setAttribute('loading', 'lazy');
         img.setAttribute('alt', `${company.name} logo`);
 
-        card.appendChild(h4);
-        card.appendChild(phone);
-        card.appendChild(address);
-        card.appendChild(site);
-        card.appendChild(img);
-    }
-
-    function randomSpawn(){
-        let openSpot = 0;
-        randomCard = Math.floor(Math.random() * buildSpotlight().length);
-
-        select_card = buildSpotlight()[randomCard]
-
-        if (openSpot == 0) {
-            let cardAd = document.querySelector('#spotlight1');
-            buildSpotlight(company, cardAd);
-        }
+        bizCard.appendChild(h4);
+        bizCard.appendChild(img);
+        bizCard.appendChild(phone);
+        bizCard.appendChild(address);
+        bizCard.appendChild(site);
+        
     }
