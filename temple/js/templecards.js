c471bbd5f397af060cfb74b23680a4d48283446e
Kiser09 <kiser09@byui.edu>
2022-04-06 03:33:17 -0800

    made more changes

--- temple/js/templecards.js
+++ temple/js/templecards.js
@@ -27,7 +27,7 @@ fetch(requestURL)
         let history = document.createElement('ol');
         let announcement = document.createElement('li');
         let openHouse = document.createElement('li');
-        let dedicatioin = document.createElement('li');
+        let dedication = document.createElement('li');
         let website = document.createElement('p');
 
         h2.textContent = `${temples.name}`;
@@ -39,19 +39,32 @@ fetch(requestURL)
         endowment.textContent = `Endowment Sessions Times: ${temples.endowment}`;
         initiatory.textContent = `Initiatory Session Times: ${temples.initiatory}`;
         closure.textContent = `Temple Closures for the year: ${temples.closure}`;
+        history.textContent = `History`;
         announcement.textContent = `Temple was announced on ${temples.announcement}`;
-        openHouse.textContent = `Public Open House: ${temples.public open house}`
-        website.innerhtml = `Website: <a href="${temples.url}">${temples.url}</a>`;
+        openHouse.textContent = `Public Open House: ${temples.house}`;
+        dedication.textContent = `Temple was dedicated on: ${temples.dedication}`;
+        website.innerhtml = `Website: <a href="${temples.site}">${temples.url}</a>`;
 
         //Temple pictures
         picture.setAttribute('src', `${temples.image}`);
         picture.setAttribute('loading', 'lazy');
         picture.setAttribute('alt', `${temples.name}`);
 
+        history.appendChild(announcement);
+        history.appendChild(openHouse);
+        history.appendChild(dedication);
+
         cards.appendChild(h2);
         cards.appendChild(picture);
         cards.appendChild(phone);
         cards.appendChild(address);
+        cards.appendChild(services);
+        cards.appendChild(sessions);
+        cards.appendChild(baptism);
+        cards.appendChild(endowment);
+        cards.appendChild(initiatory);
+        cards.appendChild(closure);
+        cards.appendChild(history);
         cards.appendChild(website);
 
         templecard.prepend(cards);
