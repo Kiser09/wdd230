904a071065ae1cd13df11e74a1317633c574471e
Kiser09 <kiser09@byui.edu>
2022-04-05 23:09:03 -0800

    made more changes to the site

--- temple/js/hamburger.js
+++ temple/js/hamburger.js
@@ -4,3 +4,31 @@ function toggleMenu(){
 
 const y = document.getElementById('menubtn');
 y.onclick = toggleMenu;
+
+/****************************************************JS for image Slideshow**********************************************************************/
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("slide");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
--- temple/js/templecards.js
+++ temple/js/templecards.js
@@ -1,9 +1,6 @@
-/**Spotlight section JS**/
-
 const requestURL = 'https://kiser09.github.io/wdd230/temple/data/temples.json';
 
-let templecard = document.querySelector('#templeSpotlight');
-let spaceFilled = 0;
+const templecard = document.querySelector('.templecards');
 
 fetch(requestURL)
     .then(function (response) {
@@ -11,23 +8,10 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         const temple = jsonObject['temples'];
-        const shuffleList = temple.sort((a, b) => 0.5 - Math.random());
-        shuffleList.forEach(spaces);
+        temple.forEach(displayTemples);
     });
 
-    function spaces(house) {
-        if (spaceFilled == 1)
-            return;
-        else {
-            if (spaceFilled == 0) {
-                let emptySpace = document.querySelector('#templeSpotlight');
-                displayTemples(house, emptySpace);
-            }
-            spaceFilled++;
-        }
-    }
-
-    function displayTemples(temples, index) {
+    function displayTemples(temples) {
         //variables
         let cards = document.createElement('section');
         let h2 = document.createElement('h2');
@@ -35,18 +19,18 @@ fetch(requestURL)
         let address = document.createElement('p');
         let phone = document.createElement('p');
         let list1 = document.createElement('ol');
-        let history = document.createElement('li');
+        let session = document.createElement('li');
         let list2 = document.createElement('ol');
         let closure = document.createElement('li');
         let website = document.createElement('p');
-
-        cards.setAttribute('class', `spot${index}`);
 
         h2.textContent = `${temples.name}`;
         address.textContent = `Address: ${temples.address}`;
         phone.textContent = `Phone: ${temples.phone}`;
         website.innerhtml = `Website: <a href="${temples.url}">${temples.url}</a>`;
-        history.textContent = `History: ${temples.history}`
+
+
+        
 
         //Temple pictures
         picture.setAttribute('src',temples.image);
@@ -58,9 +42,6 @@ fetch(requestURL)
         cards.appendChild(phone);
         cards.appendChild(address);
         cards.appendChild(website);
-        list1.appendChild(history);
-        list2.appendChild(closure);
-        cards.appendChild(list1);
 
         templecard.prepend(cards);
     }
